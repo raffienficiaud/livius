aa8956ce4a9aabd1783865bbe555101753ca371e
Stephan Wenninger <stephan.wenninger@tuebingen.mpg.de>
2015-06-11 14:45:37 +0200

    Comparing Grayscale and Color Channel boundaries

--- SourceCode/video/processing/postProcessing.py
+++ SourceCode/video/processing/postProcessing.py
@@ -41,7 +41,7 @@ def get_min_max_boundaries_for_normalized_histogram(hist):
        Note: 
         The Histogram is expected to be normalized
 
-       Returns both the min and the max value for the histogram for each chanel
+       Returns both the min and the max value for the histogram
     """    
     t_min = 0
     t_max = 255
@@ -50,17 +50,15 @@ def get_min_max_boundaries_for_normalized_histogram(hist):
     max_mass = 0
 
     # Integrate until we reach 1% of the mass from each direction
-    while min_mass < 0.01:
+    while min_mass < 0.001:
         min_mass = min_mass + hist[t_min]
         t_min = t_min + 1
 
-    while max_mass < 0.01:
+    while max_mass < 0.001:
         max_mass = max_mass + hist[t_max]
         t_max = t_max - 1
 
     return t_min, t_max
-
-
 
 def get_histograms(clip):
     hist_dir = os.path.join(os.getcwd(), 'histograms')
@@ -86,11 +84,6 @@ def get_histograms(clip):
             hist_gray = cv2.calcHist([grayscale], [0], None, [256], [0,256])
 
             # Normalize
-            # hist_blue = hist_blue.astype(float32) / (frame.shape[0] * frame.shape[1])
-            # hist_green = hist_green.astype(float32) / (frame.shape[0] * frame.shape[1])
-            # hist_red = hist_red.astype(float32) / (frame.shape[0] * frame.shape[1])
-
-            # Normalize
             hist_blue = cv2.normalize(hist_blue)
             hist_green = cv2.normalize(hist_green)
             hist_red = cv2.normalize(hist_red)
@@ -104,33 +97,50 @@ def get_histograms(clip):
             min_val = min(min_blue, min_green, min_red)
             max_val = max(max_blue, max_green, max_red) 
 
-            print min_val, max_val
+            print "Min Frame :", frame[:,:,0].min()
+            print "Min Frame :", frame[:,:,1].min()
+            print "Min Frame :", frame[:,:,2].min()
 
-            framecorrected = 255.0 * (np.maximum(frame.astype(float32) - min_val, np.zeros(frame.shape)))
+            print "min and max of each channel boundary:", min_val, max_val
+
+            framecorrected = 255.0 * (np.maximum(frame.astype(float32) - min_val, 0))
+            # framecorrected = 255.0 * (frame.astype(float32) - min_val)
             framecorrected = framecorrected / (max_val - min_val)
+
             framecorrected = np.minimum(framecorrected, 255.0)
 
             framecorrected = framecorrected.astype(uint8)
 
 
             # This tries out using the grayscale image
-            # min_val, max_val = get_min_max_boundaries_for_normalized_histogram(hist_gray)
 
-            # print min_val, max_val
+            min_val, max_val = get_min_max_boundaries_for_normalized_histogram(hist_gray)
 
-            # compare = 255.0 * (np.maximum(frame.astype(float32) - min_val, np.zeros(frame.shape)))
-            # compare = compare / (max_val - min_val)
-            # compare = np.minimum(compare, 255.0)
-            # compare = compare.astype(uint8)
+
+            print "Min Frame :", grayscale.min()
+            print "grayscale", min_val, max_val
+
+            compare = 255.0 * (np.maximum(frame.astype(float32) - min_val, np.zeros(frame.shape)))
+            compare = compare / (max_val - min_val)
+            compare = np.minimum(compare, 255.0)
+            compare = compare.astype(uint8)
 
 
             # This shows the current picture and the color correction in a window
             fig = figure()
-            fig.add_subplot(2,1,1)            
-            plt.imshow(frame)
 
-            fig.add_subplot(2,1,2)
+            fig.add_subplot(3,1,1)
+            plt.imshow(frame)     
+            plt.title("Original Frame")       
+
+            fig.add_subplot(3,1,2)            
+            plt.imshow(compare)
+            plt.title("Color correction using the 1 and 99 percentile of the grayscale histogram")
+
+            fig.add_subplot(3,1,3)
             plt.imshow(framecorrected)
+            plt.title("Color correction using min and max of all color channel percentile boundaries")
+
 
             plt.show()            
             # cv2.destroyAllWindows()
