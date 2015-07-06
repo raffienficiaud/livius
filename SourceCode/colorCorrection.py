4e668c0ff547ebcaa31404e0a40998d343133612
Stephan Wenninger <stephan.wenninger@tuebingen.mpg.de>
2015-07-06 10:13:52 +0200

    slide summary update

--- SourceCode/colorCorrection.py
+++ SourceCode/colorCorrection.py
@@ -237,17 +237,21 @@ def visualize_environment_changes_and_histogram_interpolation(path_to_video, pat
     plt.show()
 
 
-def show_slide_summary(final_slide_clip, segments):
+def get_slide_summary(final_slide_clip, segments, output_shape=(7,6), slide_size=(256,160)):
+    """Gets the first frame of each computed segment and saves it to one or more summary images.
+
+       Returns an array of all the summary images.
+    """
 
     summary_images = []
 
-    COLUMNS = 7
-    ROWS = 6
-    resized_x = 256
-    resized_y = 160
+    columns = output_shape[0]
+    rows = output_shape[1]
+    resized_x = slide_size[0]
+    resized_y = slide_size[1]
 
-    summary_image_y = ROWS * resized_y
-    summary_image_x = COLUMNS * resized_x
+    summary_image_y = rows * resized_y
+    summary_image_x = columns * resized_x
 
     def new_summary_image():
         return np.zeros((summary_image_y, summary_image_x, 3), dtype=np.uint8)
@@ -255,19 +259,21 @@ def show_slide_summary(final_slide_clip, segments):
     summary_images.append(new_summary_image())
 
     image_count = 0
+    summary_count = 1
 
     for (start, end) in segments:
-        # Extract and resize
-
-        if image_count > COLUMNS * ROWS:
+        if image_count >= columns * rows:
             summary_images.append(new_summary_image())
+            image_count = 0
 
+        # Extract and resize frame
         frame = final_slide_clip.get_frame(start)
         frame = cv2.resize(frame, dsize=(resized_x, resized_y))
 
         summary_image = summary_images[-1]
 
-        pos_y, pos_x = divmod(image_count, COLUMNS)
+        # Determine position in image
+        pos_y, pos_x = divmod(image_count, columns)
 
         start_x = pos_x * resized_x
         start_y = pos_y * resized_y
@@ -275,17 +281,13 @@ def show_slide_summary(final_slide_clip, segments):
         end_x = start_x + resized_x
         end_y = start_y + resized_y
 
+        # Copy resized frame to summary image
         summary_image[start_y : end_y, start_x : end_x, :] = np.copy(frame)
 
         image_count += 1
 
-
-    # Show result image
-    plt.figure()
-    plt.imshow(summary_images[0])
-    plt.show()
-
     return summary_images
+
 
 
 if __name__ == '__main__':
@@ -297,4 +299,11 @@ if __name__ == '__main__':
 
     slides = VideoFileClip(os.path.join('Example Data', 'video_7_slides.mp4'))
 
-    show_slide_summary(slides, segments)
+    summaries = get_slide_summary(slides, segments, output_shape=(2,2))
+
+    import Image
+
+    for i, image in enumerate(summaries):
+        im = Image.fromarray(image)
+        im.save(os.path.join('Example Data',
+                             'summary' + str(i) + '.png'))
