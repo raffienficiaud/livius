2d625506bfe850ab1d51f73460a3b11c4f217059
Raffi Enficiaud <raffi.enficiaud@tuebingen.mpg.de>
2015-07-09 12:01:14 +0200

    working on the preparation

--- SourceCode/video/processing/preparation.py
+++ SourceCode/video/processing/preparation.py
@@ -11,29 +11,72 @@ import os
 import time
 import json
 import subprocess
+import logging
+
+logger = logging.getLogger()
+logger.setLevel(logging.DEBUG)
 
 
 class Preparation(object):
     
     name = "dummy_prepare"
-    filename = name + '.json'
-    
+    attributes_to_serialize = []
+        
     def __init__(self, *args, **kwargs):
         
         for name, value in kwargs.items():
             setattr(self, name, value)
+            
+        if 'json_filename' not in kwargs:
+            self.json_filename = None
     
-    def is_preparation_needed(self):
+    def is_run_up_to_date(self):
+        """Indicates that this step should be processed again"""
         return False
-     
-    def action_video(self, video_filename):
-        """Runs an action on the video file"""
+    
+    def are_states_equal(self):
+        """Returns True is the state of the current object is the same as the one in the serialized json dump"""
+        if self.json_filename is None:
+            return False
+        
+        if not os.path.exists(self.json_filename):
+            return False
+        
+        dict_json = json.load(open(self.json_filename))
+        
+        try:
+            for k in self.attributes_to_serialize:
+                if int(dict_json[k]) != str(getattr(self, k)):
+                    return False
+        
+        except KeyError:
+            return False
+        
+        except Exception, e:
+            logger.debug("Exception caught in 'are_states_equal: %r", e)
+            return False
+        
+        return True
+        
+    
+    def serialize_state(self):
+        """Flushes the state of the runner into the json file mentioned by self.json_filename"""
+        
+        if self.json_filename is None:
+            return
+        
+        d = {}
+        for k in self.attributes_to_serialize:
+            d[k] = getattr(self, k)
+        
+        with open(self.json_filename, 'w') as f:
+            json.dump(d, f)
+        
+    
+    def run(self):
+        """Runs the action"""
         pass
     
-    def action_frame(self, frame, time_frame):
-        """Runs an action on the provided frame"""
-        pass
-
 
 
 def extract_thumbnails(video_file_name, output_width, output_folder):
@@ -57,46 +100,75 @@ def extract_thumbnails(video_file_name, output_width, output_folder):
 
 class ExtractThumbnails(Preparation):
     
-    def __init__(self, *args, **kwargs):
-        super(self, ExtractThumbnails).__init__(*args, **kwargs)
-
-
-    def action_video(self, video_file_name):
-        thumbnail_path = os.path.join(os.path.dirname(video_file_name), 'thumbnails')
-        if not os.path.exists(thumbnail_path):
-            os.makedirs(thumbnail_path)
-
-        extract_thumbnails(video_file_name=video_file_name, 
-                           output_width=640, 
-                           output_folder=thumbnail_path)
-
-
-
-
-
-def prepare(video_file_name):
+    name = "thumbnails_generation"
+    attributes_to_serialize = ['video_filename', 'video_fps', 'video_width', 'output_location']
     
-    
-    file_preprocessing = os.path.splitext(video_file_name)[0] + '.json'
-    if not file_preprocessing:
-        prepare_dict = {}
-        prepare_dict['thumbnails_extracted'] = False
-    else:
-        with open(file_preprocessing) as f:
-            prepare_dict = json.load(f)
-             
-            
-    # should we extract the thumbnails
-    do_extract_thumbnails = not prepare_dict['thumbnails_extracted']
-    if do_extract_thumbnails:
-        thumbnail_path = os.path.join(os.path.dirname(video_file_name), 'thumbnails')
-        if not os.path.exists(thumbnail_path):
-            os.makedirs(thumbnail_path)
-        extract_thumbnails(video_file_name, 
-                           output_folder=thumbnail_path)
-        prepare_dict['thumbnails_extracted'] = True
+    def __init__(self, 
+                 video_filename,
+                 video_width = None,
+                 video_fps = None, 
+                 output_location = None,
+                 *args, 
+                 **kwargs):
+        """
+        :param output_location: absolute location of the generated thumbnails
+        """
         
-        with open(file_preprocessing, 'w') as f:
-            json.dump(prepare_dict, f)
+        super(self, ExtractThumbnails).__init__(*args, **kwargs)
+        
+        if video_filename is None:
+            raise RuntimeError("The video file name cannot be empty")
+        
+        if video_width is None:
+            video_width = 640
+
+        if video_fps is None:
+            video_fps = 1
             
+        if output_location is None:
+            output_location = os.path.join(os.path.dirname(video_filename), 'thumbnails')
+        
+        self.video_filename = os.path.abspath(video_filename)
+        self.video_fps = video_fps
+        self.video_width = video_width
+        self.output_location = output_location
+        self.json_filename = os.path.splitext(video_filename)[0] + '_' + self.name + '.json'
+
+    def is_run_up_to_date(self):
+        
+        if not os.path.exists(self.output_location):
+            return False
+        
+        return self.are_states_equal()
+
+    def run(self):
+        if not os.path.exists(self.output_location):
+            os.makedirs(self.output_location)
+
+        extract_thumbnails(video_file_name=self.video_filename, 
+                           output_width=self.video_width, 
+                           output_folder=self.output_location)
+
+        # commit to the json dump
+        self.serialize_state()
+        
+
+def prepare(video_filename):
+    
+    
+    obj_extract = ExtractThumbnails(video_filename=video_filename,
+                                    video_width=640,
+                                    video_fps=1)
+    
+    if not obj_extract.is_run_up_to_date():
+        obj_extract.run()
+
+
+
+if __name__ == '__main__':
+
+    storage = '/media/renficiaud/linux-data/'
+    filename = 'BlackmagicProductionCamera 4K_1_2015-01-16_1411_C0000.mov'
+    
+    prepare(os.path.join(storage, filename))
     
